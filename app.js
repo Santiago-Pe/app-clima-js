c4685e50a5c0547d1bc57f795cca4a024e308924
Santiago-pe <penasantiago346@gmail.com>
2022-06-28 23:59:00 -0300

    layout-first-data-calling

--- app.js
+++ app.js
@@ -3,29 +3,30 @@ const API_KEY = '9b4e7ed444bed4de85995a1dabc78df8';
 
 //Functions
 
-const fetchData = position =>{
+const fetchData = async (position) =>{
 
     // console.log(position.coords.latitude)
-    const {latitude, longitude} = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-        .then(resp => resp.json())
-        .then(data => console.log(data))
+    // const {latitude, longitude, units} = position.coords;
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lon=${units}&appid=${API_KEY}&units=metric`)
+    //     .then(resp => resp.json())
+    //     .then(data => console.log(data))
 
 
-    console.log(position)
-    // try {
-    //     loadingData(true);
-    //     const {latitude, longitude} = position.coords;
-    //     const resp = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
-    //     const data = await resp.json();
+    // console.log(position)
+    try {
+        loadingData(true);
+        const {latitude, longitude} = position.coords;
+        const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+        const data = await resp.json();
 
 
-    //     console.log(data)
-    // } catch (error) {
-    //     console.log(error);
-    // } finally{
-    //     loadingData(false);
-    // }
+        console.log(data);
+        console.log(position);
+    } catch (error) {
+        console.log(error);
+    } finally{
+        loadingData(false);
+    }
 }
 const loadingData = state => {
     state ? console.log('Cargando data') : console.log('Cargado');
