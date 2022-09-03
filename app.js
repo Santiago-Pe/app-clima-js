dac750aa79a1d045eb76484fdfabc88f005cd687
Santiago-pe <penasantiago346@gmail.com>
2022-09-02 22:38:01 -0300

    Upgrade Print Data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Const & Variables
 const API_KEY = '9b4e7ed444bed4de85995a1dabc78df8';
-// const preloader = document.getElementById('preolader');
+const preloader = document.getElementById('preolader');
 const  contentWeather = document.getElementById('dynamic-content');
 const btnSearch = document.getElementById('search');
 const input = document.getElementById('input');
@@ -28,7 +28,7 @@ const fetchData = async (city) => {
 }
 // Preloader
 const loadingData = state => {
-    state ? console.log('Cargando...') : console.log('Finalizó la carga');
+    state ? preloader.classList.remove('d-none') : preloader.classList.add('d-none');
 }
 
 // Pintar informacion en DOM
@@ -45,22 +45,31 @@ const printData = (data) => {
         humidity: data.main.humidity,
         // pressure: data.main.pressure,
         temperature: data.main.temp,
-        // tempMax: data.main.temp_max,
-        // tempMin: data.main.temp_min,
-        // feelsLike: data.main.feels_like,
+        tempMax: data.main.temp_max,
+        tempMin: data.main.temp_min,
+        feelsLike: data.main.feels_like,
         wind: data.wind.speed,
         icon: data.weather[0].icon,
-        // country: data.sys.country,
-        // date: getDate(),
+        country: data.sys.country,
+        date: getDate(),
     }
     const clone = templateWeather.cloneNode(true);
 
-    clone.getElementById('location').textContent = `Weather in ${dataWeather.location}`;
+
+    clone.getElementById('date').textContent = dataWeather.date;
+    clone.getElementById('location').textContent = `Weather in ${dataWeather.location}, ${dataWeather.country}`;
     clone.getElementById('icon').scr = `https://openweathermap.org/img/wn/${dataWeather.icon}.png`;
+
     clone.getElementById('description').textContent = `${dataWeather.description}`;
     clone.getElementById('temperature').textContent = `${dataWeather.temperature} ºC`;
-    clone.getElementById('humidity').textContent = `Humidity: ${dataWeather.humidity}`;
-    clone.getElementById('wind').textContent = `Wind Speed: ${dataWeather.wind}`;
+    clone.getElementById('humidity').textContent = `Humidity:  ${dataWeather.humidity}`;
+    clone.getElementById('wind').textContent = `Wind Speed:  ${dataWeather.wind}`;
+
+    clone.getElementById('min-temp').textContent = `Min temperature:  ${dataWeather.tempMin}`;
+    clone.getElementById('max-temp').textContent = `Max temperature:  ${dataWeather.tempMax}`;
+    clone.getElementById('feels-like').textContent = `Feels Like:  ${dataWeather.feelsLike}`;
+
+
     
     fragment.appendChild(clone)
     contentWeather.appendChild(fragment)
@@ -79,7 +88,6 @@ const getDate = () => {
 const searchWeather = () =>{
     contentWeather.textContent = "";
     fetchData(input.value) 
-    // console.log(input.value)
 }
 
 // Dispara el evento el click en el boton
